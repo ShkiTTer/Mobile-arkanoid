1d7b1d1efbaa68732149d7badec417c3e864ac5a
ShkiTTer <shkipper65@gmail.com>
2019-12-06 21:46:38 +0700

    Add brick

--- app/src/main/java/com/example/game/presentation/common/GameObject.kt
+++ app/src/main/java/com/example/game/presentation/common/GameObject.kt
@@ -7,15 +7,10 @@ import android.graphics.Canvas
 abstract class GameObject(private val bitmap: Bitmap) {
     var x = 0
     var y = 0
-    var width = 0
-    var height = 0
+    val width: Int = bitmap.width
+    val height: Int = bitmap.height
     protected val screenWidth = Resources.getSystem().displayMetrics.widthPixels
     protected val screenHeight = Resources.getSystem().displayMetrics.heightPixels
-
-    init {
-        width = bitmap.width
-        height = bitmap.height
-    }
 
     fun draw(canvas: Canvas) {
         canvas.drawBitmap(bitmap, x.toFloat(), y.toFloat(), null)
--- app/src/main/java/com/example/game/presentation/ui/GameView.kt
+++ app/src/main/java/com/example/game/presentation/ui/GameView.kt
@@ -20,6 +20,8 @@ class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback 
     private val player = Player(context, context.getDrawable(R.drawable.paddle)!!.toBitmap())
     private val ball = Ball(context.getDrawable(R.drawable.ball)!!.toBitmap())
     private val bricks = mutableListOf<Brick>()
+    //private val brickRows: List<List<Int>>
+    //private val brickCols: List<Int>
 
     private val screenWidth = Resources.getSystem().displayMetrics.widthPixels
     private val screenHeight = Resources.getSystem().displayMetrics.heightPixels
@@ -30,6 +32,14 @@ class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback 
         gameThread = GameThread(holder, this)
         setZOrderOnTop(true)
         holder.setFormat(PixelFormat.TRANSPARENT)
+
+        val brickImage = context.getDrawable(R.drawable.element_green_rectangle)!!.toBitmap()
+
+        val brick = Brick(brickImage, 1).apply {
+            x = screenWidth / 3
+            y = width
+        }
+        bricks.add(brick)
     }
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
@@ -62,6 +72,16 @@ class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback 
     fun update() {
         player.update()
         ball.update()
+
+        if (ball.y + ball.height >= screenHeight) {
+            gameThread.stop()
+        }
+
+        if (ball.y + ball.height >= player.y) {
+            if (ball.x + ball.width / 2 >= player.x && ball.x + ball.width / 2 <= player.x + player.width) {
+                ball.reverceVelocityY()
+            }
+        }
     }
 
     fun startGame() {
