0dafbbf2ceafa14200ab98a85b9b4b51d7218f57
ShkiTTer <shkipper65@gmail.com>
2019-12-17 13:10:44 +0700

    Add menu and map activities

---
+++ app/src/main/java/com/example/game/presentation/ui/MapActivity.kt
@@ -0,0 +1,29 @@
+package com.example.game.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.game.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.isMyLocationEnabled = true
+    }
+}
